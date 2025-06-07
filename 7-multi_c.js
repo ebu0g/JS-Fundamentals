2c45982479fdd722e54bbbd8ba63b8827b14c851
ebu0g <gebawak@gmail.com>
2025-06-07 23:54:58 +0300

    7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,7 @@
-const args = process.argv.slice(2);
-const count = parseInt(args[0]);
+const count = parseInt(process.argv[2]);
 
-if (isNaN(count) || count <= 0) {
-  console.log("Missing number of occurrences");
-} else {
-  let i = 0;
-  while (i < count) {
+if (!isNaN(count) && count > 0) {
+  for (let i = 0; i < count; i++) {
     console.log("C is fun");
-    i++;
   }
 }
