935052e81450a1e17d5b63c90e765f5364a79e8b
Eb G <gebawak@gmail.com>
2025-08-16 23:26:48 +0300

    .

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,10 @@
-const arg = process.argv[2];
-if (arg === undefined) {
-  console.log("No argument");
-} else {
-  console.log(arg);
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("HBTN");
+} else if (args.length === 1) {
+  console.log(args[0]);
+} else if (args.length === 2) {
+  console.log(`${args[0]} ${args[1]}`);
 }
+
