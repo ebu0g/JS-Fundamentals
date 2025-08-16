6e1b25f15b90292fc821e092da6c11ab8063fddf
Eb G <gebawak@gmail.com>
2025-08-16 23:18:03 +0300

    update

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,14 @@
 function argument(arg1, arg2){
-    return arg1 + " " + arg2;
+    if (arg1 && arg2) {
+        return arg1 + " " + arg2;
+    } else if (arg1) {
+        return "HBTN";
+    } else {
+        return "";
+    }
 }
 
 result = argument("python" + " fun");
 console.log(result);
+console.log(argument("Python")); 
+console.log(argument()); 
