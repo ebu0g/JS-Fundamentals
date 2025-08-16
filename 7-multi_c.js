643b52fedd9fa88746079a5d6c8efe1932749ddf
Eb G <gebawak@gmail.com>
2025-08-17 01:05:45 +0300

    .

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,10 @@
 const x = parseInt(process.argv[2]);
 
-if (!x || x <= 0) {
-    console.log("Missing number of occurrences");
-} else {
+if (x > 0) {
     let i = 0;
     while (i < x) {
         console.log("C is fun");
         i++;
     }
 }
+
